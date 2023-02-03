062de031f332f7542c605917f65214096ddb825d
Gleb <gleb.alef@gmail.com>
2023-02-03 14:35:54 +0000

    добавлены: - контроллеры Ads Image

--- src/main/java/ru/skypro/homework/dto/CommentDto.java
+++ src/main/java/ru/skypro/homework/dto/CommentDto.java
@@ -3,10 +3,9 @@ package ru.skypro.homework.dto;
 import lombok.Data;
 
 @Data
-public class Ads {
+public class CommentDto {
     private int author;
-    private String[] image;
+    private String createdAt;
     private int pk;
-    private int price;
-    private String title;
+    private String text;
 }
