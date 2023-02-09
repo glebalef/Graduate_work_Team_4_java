819b77e15557c04fe259a7614b47b7493e243d32
Gleb <gleb.alef@gmail.com>
2023-02-09 10:42:06 +0000

    добавлены: - контроллеры Ads Image

--- src/main/java/ru/skypro/homework/dto/ResponseWrapperAdsDto.java
+++ src/main/java/ru/skypro/homework/dto/ResponseWrapperAdsDto.java
@@ -4,10 +4,11 @@ import lombok.Data;
 import ru.skypro.homework.model.Ads;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Data
 public class ResponseWrapperAdsDto {
     private int count;
-    private ArrayList <Ads> results;
+    private List<Ads> results;
 
 }
--- src/main/java/ru/skypro/homework/model/Ads.java
+++ src/main/java/ru/skypro/homework/model/Ads.java
@@ -1,4 +1,22 @@
 package ru.skypro.homework.model;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Ads {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int pk;
+    private String authorFirstName;
+    private String authorLastName;
+    private String description;
+    private String email;
+    @ElementCollection
+    private ArrayList<String> image;
+    private String phone;
+    private int price;
+    private String type;
 }
