81116ecb06b46b646459ac782b59af00782b3e30
EvgenyFisenkoJava <evgeny.f.aircelle@gmail.com>
2023-02-25 20:47:45 +0200

    переделал Authorization and Authentication

--- src/main/java/ru/skypro/homework/repository/AdsRepository.java
+++ src/main/java/ru/skypro/homework/repository/AdsRepository.java
@@ -2,7 +2,6 @@ package ru.skypro.homework.repository;
 
 import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.skypro.homework.entity.Ads;
 
@@ -12,13 +11,7 @@ import java.util.List;
 public interface AdsRepository extends JpaRepository<Ads, Long> {
 
     List<Ads> findAllByUserInfoId(Long id);
-
     @NotNull List<Ads> findAll();
-
     List<Ads> findAdsByTitleOrDescriptionContainingIgnoreCase(String part1, String part2);
-
-    @Query(value = "SELECT user_info_id from ads", nativeQuery = true)
-    Long findUserInfoId(Long id);
-
 
 }
--- src/main/java/ru/skypro/homework/repository/CommentRepository.java
+++ src/main/java/ru/skypro/homework/repository/CommentRepository.java
@@ -1,7 +1,6 @@
 package ru.skypro.homework.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import ru.skypro.homework.entity.Comment;
 
 import javax.validation.constraints.NotNull;
@@ -9,8 +8,5 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     @NotNull List<Comment> findCommentsByAds_Pk(Long ads_pk);
-
-    @Query(value = "SELECT user_info_id from comments", nativeQuery = true)
-    Long findUserInfoId(Long commentId);
 
 }
