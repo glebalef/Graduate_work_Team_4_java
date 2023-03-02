8a321588fed88fd662b44b10682a025610d5255b
EvgenyFisenkoJava <evgeny.f.aircelle@gmail.com>
2023-03-02 18:04:45 +0200

    recommit avatar upload/update

--- src/main/java/ru/skypro/homework/controller/UserController.java
+++ src/main/java/ru/skypro/homework/controller/UserController.java
@@ -14,7 +14,6 @@ import org.springframework.web.multipart.MultipartFile;
 import ru.skypro.homework.dto.NewPasswordDto;
 import ru.skypro.homework.dto.UserDto;
 import ru.skypro.homework.entity.UserInfo;
-import ru.skypro.homework.repository.AvatarRepository;
 import ru.skypro.homework.service.AuthService;
 import ru.skypro.homework.service.UserService;
 
@@ -27,7 +26,6 @@ import java.io.IOException;
 public class UserController {
     private final UserService userService;
     private final AuthService authService;
-
 
 
     @PostMapping("/set_password")
@@ -100,13 +98,6 @@ public class UserController {
         userService.updateUserImage(image, authentication.getName());
         return ResponseEntity.ok().build();
     }
-
-
-    //    @GetMapping(value = "/me/image/{id}/", produces = {MediaType.IMAGE_PNG_VALUE})
-//    public byte[] getAvatrar(@PathVariable Long id) {
-//     UserDto user = userService.ge;
-//        return user.getImage();
-//    }
     @GetMapping(value = "/{id}/image", produces = {MediaType.IMAGE_PNG_VALUE})
     public byte[] getImage(@PathVariable("id") Long id) throws IOException {
         return userService.getImage(id);
