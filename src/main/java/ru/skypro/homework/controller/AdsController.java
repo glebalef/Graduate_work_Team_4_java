1428e4e3d51c1c0a887f16a5f695a29fdf52beb3
EvgenyFisenkoJava <evgeny.f.aircelle@gmail.com>
2023-03-02 16:54:27 +0200

    avatar upload/update

--- src/main/java/ru/skypro/homework/controller/AdsController.java
+++ src/main/java/ru/skypro/homework/controller/AdsController.java
@@ -65,7 +65,7 @@ public class AdsController {
         imageService.uploadImage(id, image);
 
         // добавили картинку к Ads
-        List<Image> images = new ArrayList<Image>();
+        List<Image> images = new ArrayList<>();
         images.add(imageService.findImage(id));
         Ads reWrite = Objects.requireNonNull(adsService.getAdsNotDtoById(id));
         reWrite.setImage(images);
